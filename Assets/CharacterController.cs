9645c19c893418474f8c8f83da75ceab864240ed
Byron Crowhurst <57679765+ByPossum@users.noreply.github.com>
2020-03-09 21:56:53 +0000

    Messed around with Clamping but broke so I commented it out.

--- Assets/CharacterController.cs
+++ Assets/CharacterController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CharacterController : MonoBehaviour
 {
     [SerializeField] float f_offset;
+    [SerializeField] float f_maxoffset;
     [SerializeField] Transform lookat;
     // Start is called before the first frame update
     void Start()
@@ -15,10 +16,9 @@ public class CharacterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetAxisRaw("Horizontal") != 0)
-            transform.position += (new Vector3((Input.GetAxisRaw("Horizontal") * f_offset) * Time.deltaTime, 0, 0));
-        if (Input.GetAxisRaw("Vertical") != 0)
-            transform.position += (new Vector3(0, (Input.GetAxisRaw("Vertical") * f_offset) * Time.deltaTime, 0));
+        transform.position += new Vector3(Input.GetAxisRaw("Horizontal") * f_offset * Time.deltaTime, 0, 0);
+        transform.position += new Vector3(0, Input.GetAxisRaw("Vertical") * f_offset * Time.deltaTime, 0);
+        //transform.position = new Vector3(Mathf.Clamp(transform.position.x, -f_maxoffset, f_maxoffset), Mathf.Clamp(transform.position.y, -f_maxoffset, f_maxoffset), Mathf.Clamp(transform.position.z, -f_maxoffset, f_maxoffset));
         transform.LookAt(lookat);
     }
 }
