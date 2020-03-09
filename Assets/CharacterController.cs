636d07107e5d85ae2d94be4dd4684eb64d62e3c0
Byron Crowhurst <57679765+ByPossum@users.noreply.github.com>
2020-03-09 20:02:40 +0000

    Character Controller added with lookat point in window

---
+++ Assets/CharacterController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    [SerializeField] float f_offset;
+    [SerializeField] Transform lookat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxisRaw("Horizontal") != 0)
+            transform.position += (new Vector3((Input.GetAxisRaw("Horizontal") * f_offset) * Time.deltaTime, 0, 0));
+        if (Input.GetAxisRaw("Vertical") != 0)
+            transform.position += (new Vector3(0, (Input.GetAxisRaw("Vertical") * f_offset) * Time.deltaTime, 0));
+        transform.LookAt(lookat);
+    }
+}
