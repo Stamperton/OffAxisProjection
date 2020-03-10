a7b595990e366cd0aebae4a74d1ae90f2cdef37d
Byron Crowhurst <57679765+ByPossum@users.noreply.github.com>
2020-03-10 17:43:36 +0000

    Character controller now uses offaxis projection & (technically) is clamped

--- Assets/CharacterController.cs
+++ Assets/CharacterController.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Assets.CameraOffAxisProjection.Scripts;
 
 public class CharacterController : MonoBehaviour
 {
     [SerializeField] float f_offset;
     [SerializeField] float f_maxoffset;
     [SerializeField] Transform lookat;
+    [SerializeField] CameraOffAxisProjection offAxis;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,9 +18,6 @@ public class CharacterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(Input.GetAxisRaw("Horizontal") * f_offset * Time.deltaTime, 0, 0);
-        transform.position += new Vector3(0, Input.GetAxisRaw("Vertical") * f_offset * Time.deltaTime, 0);
-        //transform.position = new Vector3(Mathf.Clamp(transform.position.x, -f_maxoffset, f_maxoffset), Mathf.Clamp(transform.position.y, -f_maxoffset, f_maxoffset), Mathf.Clamp(transform.position.z, -f_maxoffset, f_maxoffset));
-        transform.LookAt(lookat);
+        offAxis.PointOfView = new Vector3(Input.GetAxis("Horizontal") * f_offset, Input.GetAxis("Vertical") * f_offset /* Time.deltaTime*/, 0);
     }
 }
