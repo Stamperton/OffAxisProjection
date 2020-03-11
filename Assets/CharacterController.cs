b80babf7772d930bc1c067df717ffe6ec36bb93c
Byron Crowhurst <57679765+ByPossum@users.noreply.github.com>
2020-03-11 16:36:19 +0000

    Working

--- Assets/CharacterController.cs
+++ Assets/CharacterController.cs
@@ -9,7 +9,7 @@ public class CharacterController : MonoBehaviour
     [SerializeField] CameraOffAxisProjection offAxis;
     #region We Might Still Use These
     //[SerializeField] float f_maxoffset;
-    //[SerializeField] Transform lookat;
+    [SerializeField] Transform lookat;
     #endregion
     // Start is called before the first frame update
     void Start()
@@ -20,6 +20,8 @@ public class CharacterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        offAxis.PointOfView = new Vector3(Input.GetAxis("Horizontal") * f_offset, Input.GetAxis("Vertical") * f_offset /* Time.deltaTime*/, 0);
+        lookat.rotation = new Quaternion(Input.GetAxis("Vertical") * f_offset, -Input.GetAxis("Horizontal") * f_offset, 0.0f, 1.0f);
+        //offAxis.PointOfView = new Vector3(Input.GetAxis("Horizontal") * f_offset, Input.GetAxis("Vertical") * f_offset, 0);
+        //transform.rotation = Quaternion.LookRotation(lookat.position - transform.TransformPoint(offAxis.PointOfView));
     }
 }
--- Assets/Scripts/OffAxisController.cs
+++ Assets/Scripts/OffAxisController.cs
@@ -3,36 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using Assets.CameraOffAxisProjection.Scripts;
 
+[ExecuteInEditMode]
 public class OffAxisController : MonoBehaviour
 {
-    CameraOffAxisProjection cameraView;
-    Animator anim;
+    [SerializeField] CameraOffAxisProjection cameraView;
 
     private void Start()
     {
-        anim = GetComponentInParent<Animator>();
-        cameraView = GetComponentInParent<CameraOffAxisProjection>();
     }
 
     private void Update()
     {
-        cameraView.PointOfView = this.transform.localPosition;
-        if (Input.GetAxis("Horizontal") < 0)
-        {
-            anim.SetBool("LeanRight", false);
-            anim.SetBool("LeanLeft", true);
-        }
-        else if (Input.GetAxis("Horizontal") > 0)
-        {
-            anim.SetBool("LeanLeft", false);
-            anim.SetBool("LeanRight", true);
-        }
-        else if (Input.GetAxis("Horizontal") == 0)
-        {
-            anim.SetBool("LeanLeft", false);
-            anim.SetBool("LeanRight", false);
-        }
+        cameraView.PointOfView = transform.localToWorldMatrix * transform.localPosition;
 
-        Debug.Log(Input.GetAxis("Horizontal"));
     }
 }
